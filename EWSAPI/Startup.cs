7f1a006b3e01a76e1dca4af7f05fd8e8fde2c80f
謝忠和 <coolchungho@gmail.com>
2018-07-14 12:19:50 +0800

    add swagger

--- EWSAPI/Startup.cs
+++ EWSAPI/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace EWSAPI
 {
@@ -32,6 +34,33 @@ namespace EWSAPI
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    // name: 攸關 SwaggerDocument 的 URL 位置。
+                    name: "v1",
+                    // info: 是用於 SwaggerDocument 版本資訊的顯示(內容非必填)。
+                    info: new Info
+                    {
+                        Title = "EWSAPI 文件",
+                        Version = "1.0.0",
+                        Description = "有關Early Warning Score API 的文件",
+                        TermsOfService = "None",
+                        Contact = new Contact
+                        {
+                            Name = "謝忠和",
+                            Url = "https://blog.johnwu.cc"
+                        },
+                        License = new License
+                        {
+                            Name = "CC BY-NC-SA 4.0",
+                            Url = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+                        }
+                    }
+                );
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +77,17 @@ namespace EWSAPI
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    // url: 需配合 SwaggerDoc 的 name。 "/swagger/{SwaggerDoc name}/swagger.json"
+                     "/swagger/v1/swagger.json",
+                    // description: 用於 Swagger UI 右上角選擇不同版本的 SwaggerDocument 顯示名稱使用。
+                     "RESTful API v1.0.0"
+                );
+            });
 
             app.UseMvc(routes =>
             {
